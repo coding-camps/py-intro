0112682b4aa16f42d60feea90e59f6c47c106746
cosmos-fly <coder211@163.com>
2021-04-04 10:11:42 +0800

    update code

--- src/utils/os_utils.py
+++ src/utils/os_utils.py
@@ -1,9 +1,22 @@
-# -*- encoding: utf-8 -*-
+# -*-coding:utf-8-*-
 
 import os
 from os import path
 
-def cls_mac_temp(root_path):
+
+def cls_mac_temp(root_path: str):
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            if file.startswith('.'):
+                temp_file = path.join(root, file)
+                print(temp_file)
+                os.remove(temp_file)
+        # for dir in dirs:
+        #     if dir.startswith('._'):
+        #         print(os.path.join(root, dir))
+
+
+def static_file_sys(root_path):
     cnt_dir = 0
     cnt_file = 0
     for root, dirs, files in os.walk(root_path):
@@ -21,9 +34,3 @@ def cls_mac_temp(root_path):
         cnt_file += len(files)
     print("total dir  ->", cnt_dir)
     print("total file ->", cnt_file)
-
-
-if __name__ == '__main__':
-    root = r"/Users/cosmos/Desktop/工作学习资料"
-    root = r"/Users/cosmos/Desktop/微信文件"
-    cls_mac_temp(root)
