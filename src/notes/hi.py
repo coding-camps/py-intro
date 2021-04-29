d247e7f7046ba03e45f1a7c2d17eb0c8b7355658
Artin Simons <coder211@163.com>
2021-04-29 15:56:24 +0800

    remove unuseful source code

--- src/notes/hi.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-class StorageService:
-
-    def upload_file(self, file: bytes): ...
-
-
-class StorageService2(StorageService):
-    def upload_file(self, file: bytes): ...
